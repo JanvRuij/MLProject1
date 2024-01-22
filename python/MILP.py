dcb0831b9947512cca8a30a4fd045ef0c7eb567a
JanvRuij <jan.vanruijven96@gmail.com>
2024-01-22 19:08:56 +0100

    begint echt ergens op te lijken g

--- python/MILP.py
+++ python/MILP.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 nr_nodes = 8
-lam = 0.001
+lam = 0.01
 # read the data
 X = np.genfromtxt("WisconsinBreastCancerData/X.csv", delimiter=",")
 Y = np.genfromtxt("WisconsinBreastCancerData/Y.csv", delimiter=",")
@@ -12,17 +12,16 @@ Y = np.genfromtxt("WisconsinBreastCancerData/Y.csv", delimiter=",")
 # generate training, testing and validation sets
 X_train, X_test, Y_train, Y_test = train_test_split(
         X, Y, test_size=0.2, random_state=42)
-X_train, X_val, Y_train, Y_val = train_test_split(
-        X_train, Y_train, test_size=0.25, random_state=42)
 
 # number of training samples, features and large M
 N = len(Y_train)
 print(N)
 P = len(X[0])
-M = 1000000000
+M = 10000
 
 # create milp model
 model = gp.Model("MILP")
+model.setParam("Timelimit", 15)
 
 # add variables
 Y_pred = model.addVars(N, vtype=GRB.BINARY, name="Y_pred")
@@ -32,13 +31,13 @@ W_1 = model.addVars(nr_nodes, P, vtype=GRB.BINARY, name="W_1")
 Wcorrect_1 = model.addVars(nr_nodes, P, vtype=GRB.CONTINUOUS, lb=-1.1, ub=1.1, name="Wcorrect_1")
 A = model.addVars(N, nr_nodes, vtype=GRB.CONTINUOUS, lb=0, name="A")
 S = model.addVars(N, nr_nodes, vtype=GRB.CONTINUOUS, lb=0, name="S")
-Z = model.addVars(N, nr_nodes, vtype=GRB.CONTINUOUS, lb=0, ub=1, name="Z")
+Z = model.addVars(N, nr_nodes, vtype=GRB.BINARY, name="Z")
 B1 = model.addVars(nr_nodes, vtype=GRB.CONTINUOUS, name="B1")
 B2 = model.addVar(vtype=GRB.CONTINUOUS, name="B2")
 
 # add Relu constrains
 model.addConstrs((A[i, j] <= (1 - Z[i, j]) * M
-                  for i in  range(N)
+                  for i in range(N)
                   for j in range(nr_nodes)))
 
 model.addConstrs(S[i, j] <= M * Z[i, j]
@@ -60,7 +59,7 @@ model.addConstrs(Wcorrect_2[j] == W_2[j] * 2 - 1 for j in range(nr_nodes))
 for i in range(N):
     model.addConstr(gp.quicksum(
         Wcorrect_2[j] * A[i, j] for j in range(nr_nodes))
-                    + B2 <= Y_pred[i] * M)
+                    + B2 - Y_pred[i] * M <= 0)
 
 # add auxiliary variables to linearize the absolute value
 aux = model.addVars(N, vtype=GRB.CONTINUOUS, name="aux")
@@ -69,19 +68,22 @@ model.addConstrs(Y_train[i] - Y_pred[i] <= aux[i] for i in range(N))
 model.addConstrs(-Y_train[i] + Y_pred[i] <= aux[i] for i in range(N))
 # Set objective function
 model.setObjective(gp.quicksum(aux[i] for i in range(N))
-                    + lam * gp.quicksum(A[i, j] * A[i, j] for i in range(N) for j in range(nr_nodes))
+                   + lam * (gp.quicksum(A[i, j] * A[i, j]
+                   for i in range(N) for j in range(nr_nodes))
+                   + gp.quicksum(Wcorrect_1[j, p] - Wcorrect_1[j+1, p]
+                   for j in range(nr_nodes - 1) for p in range(P)) - B2
+                   + gp.quicksum(B1[j] - B1[j+1] for j in range(nr_nodes-1))
+                   + gp.quicksum(Wcorrect_2[j] - Wcorrect_2[j+1] for j in range(nr_nodes-1)))
                    , GRB.MINIMIZE)
 
 
 model.optimize()
-if model.status == GRB.OPTIMAL:
-    print("Optimal solution found.")
+if model.status == GRB.OPTIMAL or model.Status == GRB.TIME_LIMIT:
     Y_pred_values = np.array([Y_pred[i].X for i in range(N)])
-    print(Y_pred_values)
+
     # Retrieve the values of decision variables
     Wcorrect_1_values = np.array([Wcorrect_1[j, p].X for j in range(nr_nodes) for p in range(P)])
     Wcorrect_1_values = Wcorrect_1_values.reshape(8, 30)
-    print(Wcorrect_1_values)
 
     A = np.array([A[i, j].X for i in range(N) for j in range(nr_nodes)])
     A = A.reshape(N, nr_nodes)
@@ -90,7 +92,6 @@ if model.status == GRB.OPTIMAL:
 
     B1_values = np.array([B1[i].X for i in range(nr_nodes)])
     B2_value = B2.X
-
 
 elif model.status == GRB.INFEASIBLE:
     print("The model is infeasible.")
@@ -101,6 +102,7 @@ else:
 
 first_node_values = np.dot(Wcorrect_1_values, X_test.T) + B1_values[:, np.newaxis]
 Y_test_pred = np.dot(first_node_values.T, Wcorrect_2_values) + B2_value
+Y_test_pred = np.where(Y_test_pred > 0, 1, 0)
 print(Y_test_pred)
-print(np.any(Y_test_pred > 0))
-print(np.any(Y_test_pred < 0))
+print(Y_test)
+
